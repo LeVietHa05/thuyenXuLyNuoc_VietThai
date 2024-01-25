0a5b25d6e4eafd09fc5c431b0d88e89cb1576204
LeVietHa05 <ha.leviet.0504@gmail.com>
2024-01-25 15:57:41 +0700

    them nut nhan bat dau do nuoc, update socket

--- socketapi.js
+++ socketapi.js
@@ -4,15 +4,24 @@ const socketapi = {
     io: io
 }
 
+const messageID = [];
+
 io.on("connection", (socket) => {
     console.log("[INFO] new connection: [" + socket.id + "]",
         socket.request.connection.remoteAddress);
     socket.on("/esp/envir", (data) => {
         console.log(`[/esp/envir] from ${data.clientID} via socket id: ${socket.id}`);
-        socket.broadcast.emit("/web/envir", data);
+        //add check messageID, if messageID is exist, don't send to web
+        if (messageID.indexOf(data.messageID) == -1) {
+            messageID.push(data.messageID);
+            socket.broadcast.emit("/web/envir", data);
+            setTimeout(() => {
+                messageID.splice(messageID.indexOf(data.messageID), 1);
+            }, 10000);
+        }
     });
 
-    socket.on("/esp/other", (data) => { 
+    socket.on("/esp/other", (data) => {
         console.log(`[esp/other] from ${data.clientID} via socket id: ${socket.id}`);
         socket.broadcast.emit("/web/other", data);
     });
