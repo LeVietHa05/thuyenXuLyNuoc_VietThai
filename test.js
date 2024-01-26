1da97cb8aa7569be54f792044ed416be9a7dd227
LeVietHa05 <ha.leviet.0504@gmail.com>
2024-01-26 14:01:50 +0700

    fix minor bug with flow and windspeed

--- test.js
+++ test.js
@@ -14,17 +14,18 @@ setInterval(() => {
         clientID: "esp8266",
         temp: randomFloat(17, 19),
         humi: randomInt(65,70),
-        NH3: randomFloat(5, 10),
+        NH4: randomFloat(5, 10),
         co2: randomInt(300, 400),
-        co: randomInt(50, 100),
+        co: randomInt(0,10),
         dust: {
             "PM10": randomFloat(50, 55),
             "PM2.5": randomFloat(30, 35),
         },
         tds: 0,
         ph: 0,
-        windSpeed: randomInt(0, 10),
-        flow: randomInt(0, 10),
+        windSpeed: randomInt(8, 10),
+        flow: randomInt(0,0),
         tempDS18B20: 0,
+        messageID: randomInt(0, 1000),
     })
 }, 3000)
