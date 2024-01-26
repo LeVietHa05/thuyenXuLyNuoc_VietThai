acffd51b74d0a29c5ec85a0c7f55c6bd1770d2a4
LeVietHa05 <ha.leviet.0504@gmail.com>
2024-01-26 13:45:54 +0700

    fix chart data order

--- test.js
+++ test.js
@@ -5,20 +5,26 @@ function randomInt(min, max) {
     return min + Math.floor((max - min) * Math.random());
 }
 
+function randomFloat(min, max, fixed = 2) {
+    return min + (max - min) * Math.random().toFixed(fixed);
+}
+
 setInterval(() => {
     socket.emit("/esp/envir", {
         clientID: "esp8266",
-        temp: randomInt(23, 28),
-        humi: randomInt(50, 60),
-        NH4: randomInt(50, 100),
-        co2: randomInt(500, 1000),
-        co: randomInt(0, 50),
-        dust: randomInt(0, 10),
-        tds: randomInt(0, 100),
-        ph: randomInt(0, 14),
+        temp: randomFloat(17, 19),
+        humi: randomInt(65,70),
+        NH3: randomFloat(5, 10),
+        co2: randomInt(300, 400),
+        co: randomInt(50, 100),
+        dust: {
+            "PM10": randomFloat(50, 55),
+            "PM2.5": randomFloat(30, 35),
+        },
+        tds: 0,
+        ph: 0,
         windSpeed: randomInt(0, 10),
         flow: randomInt(0, 10),
-        waterLevel: randomInt(0, 10),
-        tempDS18B20: randomInt(23, 28),
+        tempDS18B20: 0,
     })
 }, 3000)
